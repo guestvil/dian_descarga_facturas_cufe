d6222be8da73c67ced380ef8854f238c0db51bdb
Guillermo Estupinan <estupinan.services@outlook.com>
2025-02-17 21:06:48 -0500

    Tested and works well, design decisions pending

--- radian.py
+++ radian.py
@@ -55,6 +55,7 @@ def get_dian_pdfs(codes_list: list, dian_website: str, playwright_page):
         downloaded_file = download_info.value
         downloaded_file.save_as(file_path)
         print('pdf downloaded')
+        # This appends relatives paths for the pdf reading function, I don't know why this works but providing absolute paths to the Downloads folder simply does not work
         files_list.append(file_name)
         playwright_page.get_by_role('link', name='Volver').click()
     return files_list
@@ -71,16 +72,33 @@ def get_payment_method(file_path_lists):
         forma_pago = invoice_text[invoice_text.index('pago:') + 1]
         invoice = pdf_path.split('.')[0]
         file_payment_method.append((invoice, forma_pago))
-    print(file_payment_method)
     return file_payment_method
+
+
+def update_excel(file_path: str, invoices_tuples: list):
+    df_excel = pd.read_excel(file_path, engine='openpyxl')
+    indexing = 0
+    df_excel['pago'] = None
+    for row in df_excel.itertuples(index=True):
+        try: 
+            if row[2] == invoices_tuples[indexing][0]:
+                print(invoices_tuples[indexing][1])
+                df_excel.at[row.Index, 'pago'] = invoices_tuples[indexing][1]
+                indexing += 1
+            else:
+                continue
+        except IndexError as e:
+            print('Programa finalizado. Hay facturas del excel sin procesar')
+    df_excel.to_excel(file_path, index=False, engine='openpyxl')
+    print('Changes saved to excel file!')
+    return None
 
 
 def main():
     path = '1_enero.xlsx'
     dian_url = load_env_files()
     invoice_list = load_invoice_codes(path)
-    # This created relatives paths, I don't know why this works but providing absolute paths to the Downloads folder simply does not work
-    invoices_paths = [invoice + '.pdf' for invoice in invoice_list]
+    invoices_not_read = []
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch_persistent_context(user_data_dir='', channel='chrome', headless=False, downloads_path='/Users/guestvil/Downloads', no_viewport=True, slow_mo=1000)
         dian_page = browser.new_page()
@@ -88,7 +106,9 @@ def main():
         payment_tuple = get_payment_method(pdfs_paths)
         for tuples in payment_tuple:
             if tuples[0] not in invoice_list:
-                print(f'Faltó este código: {tuples}')
+                invoices_not_read.append(tuples[0])
+    update_excel(file_path=path, invoices_tuples=payment_tuple)
+    print('Program successful')
 
 
 if __name__ == '__main__':
