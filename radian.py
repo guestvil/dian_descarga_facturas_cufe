2bb4e3a4e455d9abd09c9bf87a3ad906eb17fd36
Guillermo Estupinan <estupinan.services@outlook.com>
2025-02-19 20:40:40 -0500

    The program works

--- radian.py
+++ radian.py
@@ -2,6 +2,8 @@ import pandas as pd
 from patchright.sync_api import sync_playwright
 from dotenv import load_dotenv
 import pymupdf
+from datetime import datetime
+from patchright._impl._errors import TimeoutError
 import os
 
 
@@ -45,19 +47,35 @@ def get_dian_pdfs(codes_list: list, dian_website: str, playwright_page):
         file_path = download_path + '/' + file_name
         print('Loggin in DIAN website')
         playwright_page.wait_for_timeout(3000)
-        playwright_page.get_by_placeholder('Ingrese el código CUFE o UUID').fill(code)
-        # playwright_page.get_by_text('Success!').wait_for(state='visible')
-        playwright_page.get_by_role('button', name='Buscar').click()
-        # playwright_page.get_by_text('Success!').wait_for(state='visible')
-        with playwright_page.expect_download() as download_info:
-            print('Downloading pdf')
-            playwright_page.get_by_role('link', name=' Descargar PDF ').click()
-        downloaded_file = download_info.value
-        downloaded_file.save_as(file_path)
-        print('pdf downloaded')
+        attempts = 3
+        download_flag = False
+        for attempt in range(attempts):
+            try: 
+                playwright_page.get_by_placeholder('Ingrese el código CUFE o UUID').fill(code)
+                playwright_page.get_by_role('button', name='Buscar').click()
+                if playwright_page.get_by_text('Falta Token de validación de captcha').is_visible():
+                    playwright_page.wait_for_timeout(4000)
+                    playwright_page.get_by_role('button', name='Buscar').click()
+                # playwright_page.get_by_text('Success!').wait_for(state='visible')
+                with playwright_page.expect_download() as download_info:
+                    print('Downloading pdf')
+                    playwright_page.get_by_role('link', name=' Descargar PDF ').click()
+                downloaded_file = download_info.value
+                downloaded_file.save_as(file_path)
+                download_flag = True
+                print('pdf downloaded')
+                break
+            except TimeoutError as e: 
+                print(e)
+                print('Algo pasó, intentanto de nuevo. Intento', attempt, '/', attempts)
+                playwright_page.reload()
         # This appends relatives paths for the pdf reading function, I don't know why this works but providing absolute paths to the Downloads folder simply does not work
-        files_list.append(download_path+ '/'+ file_name)
-        playwright_page.get_by_role('link', name='Volver').click()
+        if download_flag == True:
+            files_list.append(download_path+ '/'+ file_name)
+            playwright_page.get_by_role('link', name='Volver').click()
+        else:
+            print('No se pudo descargar: ', code)
+            playwright_page.goto(dian_website)
     return files_list
 
 
@@ -100,6 +118,7 @@ def update_excel(file_path: str, invoices_tuples: list):
 
 
 def main():
+    print(datetime.now().time())
     path = '1_enero.xlsx'
     new_path = 'updated_file.xlsx'
     dian_url = load_env_files()
@@ -108,13 +127,13 @@ def main():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch_persistent_context(user_data_dir='', channel='chrome', headless=False, downloads_path='/Users/guestvil/Downloads', no_viewport=True, slow_mo=1000)
         dian_page = browser.new_page()
-        pdfs_paths = get_dian_pdfs(codes_list=invoice_list[0:2], dian_website=dian_url, playwright_page=dian_page)
+        pdfs_paths = get_dian_pdfs(codes_list=invoice_list, dian_website=dian_url, playwright_page=dian_page)
         payment_tuple = get_payment_method(pdfs_paths)
         for tuples in payment_tuple:
             if tuples[0] not in invoice_list:
                 invoices_not_read.append(tuples[0])
         update_excel(file_path=path, invoices_tuples=payment_tuple)
-    print('Program successful')
+    print('Program successful', datetime.now().time())
 
 
 if __name__ == '__main__':
